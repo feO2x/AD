5811978db2da2b5e01dc91ffba47c9fc4132a1ec
Kenny Pflug <kenny.pflug@live.de>
2022-10-16 13:57:32 +0200

    added factorial method

---
+++ Code/AD.Tests/Shared/CommonMath.cs
@@ -0,0 +1,32 @@
+﻿using Light.GuardClauses;
+
+namespace AD.Tests.Shared;
+
+public static class CommonMath
+{
+    public static int CalculateFactorial(int number)
+    {
+        number.MustNotBeLessThan(0);
+        return number switch
+        {
+            0 or 1 => 1,
+            2 => 2,
+            _ => CalculateFactorialChecked(number)
+        };
+    }
+
+    private static int CalculateFactorialChecked(int number)
+    {
+        checked
+        {
+            var returnValue = 2;
+            var limit = number + 1;
+            for (var i = 3; i < limit; i++)
+            {
+                returnValue *= i;
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ Code/AD.Tests/Shared/FactorialTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace AD.Tests.Shared;
+
+public static class FactorialTests
+{
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 6)]
+    [InlineData(4, 24)]
+    [InlineData(5, 120)]
+    [InlineData(6, 720)]
+    [InlineData(10, 3_628_800)]
+    public static void CalculateFactorial(int input, int expectedResult)
+    {
+        var actualResult = CommonMath.CalculateFactorial(input);
+
+        actualResult.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-1000)]
+    public static void InvalidFactorialInput(int invalidInput)
+    {
+        var act = () => CommonMath.CalculateFactorial(invalidInput);
+
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData(15)]
+    [InlineData(14)]
+    [InlineData(13)]
+    public static void ResultTooLargeForInt32(int invalidInput)
+    {
+        var act = () => CommonMath.CalculateFactorial(invalidInput);
+
+        act.Should().Throw<OverflowException>();
+    }
+}
